2e05a0274692b5e5c7a151fb9d72876df5831e13
Claudio De Angelis <claudio@enboarder.com>
2021-05-31 19:53:34 +1000

    fix: seventh test passes

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,13 @@
 const fizzbuzz = () => {
   return new Array(100).fill("").map((_, i) => {
-    if (+i % 3 === 0 || +i % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      return "fizzbuzz";
+    }
+    if (i % 3 === 0) {
       return "fizz";
+    }
+    if (i % 5 === 0) {
+      return "buzz";
     }
     return i;
   });
